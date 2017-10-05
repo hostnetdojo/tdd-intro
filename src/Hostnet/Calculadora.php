0c1ece7f734a223dc7d31ce13dc4cae6adfb8334
Henrique Rodrigues <henrique@hostnet.com.br>
2017-10-05 14:13:07 -0300

    Primeiro Teste

---
+++ src/Hostnet/Calculadora.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hostnet;
+
+class Calculadora
+{
+    public function somar($n1, $n2)
+    {
+        return 2;
+    }
+}
---
+++ tests/Hostnet/CalculadoraTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hostnet;
+
+use Hostnet\Calculadora;
+use Hostnet\Helper\TestCase;
+
+class CalculadoraTest extends TestCase
+{
+    /**
+     * Testa o método de soma da forma mais simples
+     *
+     * @covers Hostnet\Calculadora::somar()
+     * @author Henrique Rodrigues <henrique@hostnet.com.br>
+     */
+    public function testSomarUmMaisUm()
+    {
+        # Instancia a calculadora
+        $calculadora = new Calculadora();
+
+        # Efetua a operação
+        $resultado = $calculadora->somar(1, 1);
+
+        # testa o resultado
+        $this->assertEquals(2, $resultado);
+    }
+}
