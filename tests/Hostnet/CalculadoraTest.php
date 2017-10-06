7668e501bda94d323dd3f6b3de86ad69e8942f59
Henrique Rodrigues <henrique@hostnet.com.br>
2017-10-06 13:45:26 -0300

    Finalizando soma simples.

--- tests/Hostnet/CalculadoraTest.php
+++ tests/Hostnet/CalculadoraTest.php
@@ -26,20 +26,20 @@ class CalculadoraTest extends TestCase
     }
 
     /**
-     * Testa o método de soma da forma mais simples
+     * Testa o método de soma para números diferentes
      *
      * @covers Hostnet\Calculadora::somar()
      * @author Henrique Rodrigues <henrique@hostnet.com.br>
      */
-    public function testSomarUmMaisDois()
+    public function testSomarNumerosDiferentes()
     {
         # Instancia a calculadora
         $calculadora = new Calculadora();
 
         # Efetua a operação
-        $resultado = $calculadora->somar(1, 2);
+        $resultado = $calculadora->somar(3, 7);
 
         # testa o resultado
-        $this->assertEquals(3, $resultado);
+        $this->assertEquals(10, $resultado);
     }
 }
